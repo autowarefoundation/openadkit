c22d910aeea1655ff1946ca96a90d5d4e7ef48bf
Tzu-Ching Yang <shingekinocore@gmail.com>
2025-12-16 08:50:41 +0800

    Feat: expose cloud port and enhance ip display
    1. Expose port 7448 in cloud_zenoh_bridge to enable remote connections from Edge.
    2. Update cloud.sh to intelligently filter and display Host IPs (WLAN/LAN), excluding Docker bridge interfaces, to simplify Edge configuration.
    
    Signed-off-by: Tzu-Ching Yang <shingekinocore@gmail.com>

--- deployments/demos/zenoh-bridge/cloud.sh
+++ deployments/demos/zenoh-bridge/cloud.sh
@@ -7,3 +7,31 @@ source ./common.sh
 CLOUD_SERVICES="visualizer cloud_zenoh_bridge"
 
 run_compose "Cloud" "$CLOUD_SERVICES" "$@"
+if [ "$1" == "up" ] || [ -z "$1" ]; then
+    echo -e "${YELLOW}[Info]${NC} Cloud services started."
+    echo -e "       To connect from Edge, set CLOUD_IP to one of the following:"
+    
+    # Function to get IPs excluding docker/br/veth interfaces
+    get_ips() {
+        ip -o -4 addr show | awk '
+        $2 !~ /^(docker|br-|veth|lo$)/ {
+            ip = $4; sub("/.*", "", ip);
+            if (ip ~ /^140\.112\./) print "match_public " ip;
+            else if (ip ~ /^192\.168\.|^10\.|^172\./) print "match_private " ip;
+            else print "match_other " ip;
+        }'
+    }
+
+    # Process and display IPs
+    IPS=$(get_ips)
+    
+    if echo "$IPS" | grep -q "match_public"; then
+        echo -e "\n       ${GREEN}[Public/WLAN IPs]${NC}"
+        echo "$IPS" | grep "match_public" | cut -d' ' -f2 | sed 's/^/       - /'
+    fi
+
+    if echo "$IPS" | grep -q "match_private"; then
+        echo -e "\n       ${YELLOW}[Private/LAN IPs]${NC}"
+        echo "$IPS" | grep "match_private" | cut -d' ' -f2 | sed 's/^/       - /'
+    fi
+fi
